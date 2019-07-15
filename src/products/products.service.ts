4bdefc743c7449be8576b18a24cb95d80c503aa7
Gavin Quirk <gavinquirk@gmail.com>
2019-07-14 17:29:12 -0700

    update product

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -18,10 +18,37 @@ export class ProductsService {
   }
 
   getSingleProduct(productId: string) {
-    const product = this.products.find(prod => prod.id === productId);
-    if (!product) {
-      return new NotFoundException('Could not find product');
-    }
+    const product = this.findProduct(productId)[0];
     return { ...product };
+  }
+
+  updateProduct(
+    productId: string,
+    title: string,
+    description: string,
+    price: number,
+  ) {
+    const [product, index] = this.findProduct(productId);
+    const updatedProduct = { ...product };
+    if (title) {
+      updatedProduct.title = title;
+    }
+    if (description) {
+      updatedProduct.description = description;
+    }
+    if (price) {
+      updatedProduct.price = price;
+    }
+
+    this.products[index] = updatedProduct;
+  }
+
+  private findProduct(id: string): [Product, number] {
+    const productIndex = this.products.findIndex(prod => prod.id === id);
+    const product = this.products[productIndex];
+    if (!product) {
+      throw new NotFoundException('Could not find product');
+    }
+    return [product, productIndex];
   }
 }
